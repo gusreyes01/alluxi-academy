cbe43fc13d8b6c5949531e78539f30ec772aa42f
Gustavo Reyes <gusreyes01@gmail.com>
2019-06-10 18:58:12 -0500

    Added urls

--- alluxiacademy/landing/urls.py
+++ alluxiacademy/landing/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.introduccion_ds, name='index'),
+    path('python-crash-course', views.python_crash_course, name='python_crash_course'),
+    path('introduccion-data-science', views.introduccion_ds, name='introduccion_ds'),
+
 ]
--- alluxiacademy/landing/views.py
+++ alluxiacademy/landing/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
-from django.http import HttpResponse
 import sweetify
+from django.core.mail import send_mail
+from django.shortcuts import render
+from django.template.loader import render_to_string
 
 
 def send_mail_wrapper(title, template, context, recipients):
@@ -15,23 +14,34 @@ def send_mail_wrapper(title, template, context, recipients):
               html_message=html_message,
               fail_silently=False)
 
-def index(request):
 
+def _submit_form(request):
+    try:
+        email = request.POST.get('email')
+        content = request.POST.get('content')
+
+        context = {
+            'email': email,
+            'content': content,
+        }
+        send_mail_wrapper('Contacto Alluxi Academy',
+                          'landing/contact_email.html',
+                          context, ['hola@alluxi.mx'])
+
+        sweetify.success(request, '¡Gracias por tu mensaje! Nos pondremos en contacto contigo a la brevedad.')
+    except Exception as e:
+        sweetify.error(request, 'Error: ' + str(e))
+
+
+def introduccion_ds(request):
     if request.method == 'POST':
-        try:
-            email = request.POST.get('email')
-            content = request.POST.get('content')
-
-            context = {
-                'email': email,
-                'content': content,
-            }
-            send_mail_wrapper('Contacto Alluxi Academy',
-                              'landing/contact_email.html',
-                              context, ['hola@alluxi.mx'])
-
-            sweetify.success(request, '¡Gracias por tu mensaje! Nos pondremos en contacto contigo a la brevedad.')
-        except Exception as e:
-            sweetify.error(request, 'Error: ' + str(e))
+        _submit_form(request)
 
     return render(request, 'landing/index-ds.html')
+
+
+def python_crash_course(request):
+    if request.method == 'POST':
+        _submit_form(request)
+
+    return render(request, 'landing/index.html')
