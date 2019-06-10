68b51369e29729686734889f0e05c288db78c7c8
Gustavo Reyes <gusreyes01@gmail.com>
2019-06-10 18:58:42 -0500

    Urls fix

--- alluxiacademy/landing/urls.py
+++ alluxiacademy/landing/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.introduccion_ds, name='index'),
-    path('python-crash-course', views.python_crash_course, name='python_crash_course'),
-    path('introduccion-data-science', views.introduccion_ds, name='introduccion_ds'),
+    path('cursos/python-crash-course', views.python_crash_course, name='python_crash_course'),
+    path('cursos/introduccion-data-science', views.introduccion_ds, name='introduccion_ds'),
 
 ]
