95ccb33c1b3c99655dd6cfded6188b3832f7f4d6
Jesús Puga <jesuspuga01@gmail.com>
2019-09-05 17:35:01 -0500

    Created models and added to admin configuration

--- alluxiacademy/landing/admin.py
+++ alluxiacademy/landing/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Course
+from .models import Instructor
+from .models import Career
+from .models import Phase
 
 # Register your models here.
+admin.site.register(Course)
+admin.site.register(Instructor)
+admin.site.register(Career)
+admin.site.register(Phase)
--- alluxiacademy/landing/models.py
+++ alluxiacademy/landing/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Career(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    institution = models.CharField(max_length=200)
+    status = models.CharField(max_length=50)
+
+class Instructor(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    title = models.CharField(max_length=200)
+    current_job = models.CharField(max_length=200)
+    indeed_account = models.CharField(max_length=200)
+    careers = models.ManyToManyField(Career)
+    status = models.CharField(max_length=50)
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    duration_in_weeks = models.IntegerField()
+    location = models.CharField(max_length=200)
+    start_date = models.DateField()
+    start_hour = models.DateTimeField()
+    finish_hour = models.DateTimeField()
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50)
+
+class Phase(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50)
+    
